785f010855c12a97d4a16a4f8d08ddbba23b3917
Ivy Wu <ivy.wu.2003@gmail.com>
2024-05-13 15:41:23 -0400

    checked validity of saved data

--- decomposition/display_noise_map.py
+++ decomposition/display_noise_map.py
@@ -2,12 +2,26 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 import torch
+import numpy as np
 
 from robust_decomposition import normalize
+
+def check_validity(directory_path):
+  images = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f)) and not f.startswith('.')]
+  invalid_images = []
+
+  for img in images:
+    image = torch.load(os.path.join(directory_path, image_filename))
+    if np.min(image) == 0 and np.max(image) == 0:
+      invalid_images.append(img)
+
+  print(invalid_images)
 
 if __name__ == "__main__":
     directory_path = 'decomposition/robust_noise'
     image_filename = '638.png'
+
+    check_validity(directory_path)
 
     image = torch.load(os.path.join(directory_path, image_filename))
     image = normalize(torch.from_numpy(image), 255)
